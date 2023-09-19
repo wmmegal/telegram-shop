d5b7943c94be231b5ee291340c885c4cbbf174bc
wmmeg <wmmegal@gmail.com>
2023-09-19 17:14:42 +0300

    feat(site): Init php telegram sdk

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Log;
+use Psr\Http\Message\RequestInterface;
 use Telegram\Bot\Api;
 
 class TelegramController extends Controller
 {
-    public function __invoke(Api $telegram)
+    public function __invoke(Api $telegram, RequestInterface $request)
     {
-        $updates = $telegram->getWebhookUpdate();
-        Log::info(1);
+
+        $updates = $telegram->getWebhookUpdate(request:  $request);
+        Log::info(print_r($updates, true));
     }
 }
