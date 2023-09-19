60f03da745a5d5ddec8074a4b3587fcae132cfa8
wmmeg <wmmegal@gmail.com>
2023-09-19 17:30:55 +0300

    feat(site): Init php telegram sdk

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Log;
-use Psr\Http\Message\RequestInterface;
 use Telegram\Bot\Api;
 
 class TelegramController extends Controller
