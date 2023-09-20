d3394640bb63e4df6911845068282423037e2c5e
wmmeg <wmmegal@gmail.com>
2023-09-20 14:57:14 +0300

    feat(commands): Start command

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -5,15 +5,31 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Log;
 use Telegram\Bot\Api;
-use Telegram\Bot\Laravel\Facades\Telegram;
+use Telegram\Bot\Exceptions\TelegramSDKException;
 
 class TelegramController extends Controller
 {
+    /**
+     * @throws TelegramSDKException
+     */
     public function __invoke(Api $telegram)
     {
         $updates = $telegram->getWebhookUpdate();
-        Log::info(print_r($updates->getChat(), true));
-        Log::info(print_r($updates->getMessage(), true));
+        $text = $updates->getMessage()['text'];
+        $chatId = $updates->getMessage()['id'];
+
+        match (true) {
+            $text === '/start' => $telegram->sendMessage([
+                'chat_id' => $chatId,
+                'reply_markup' => [
+                    'inline_keyboard' => [
+                        [
+                            ['text' => 'Открыть магазин', 'web_app' => ['url' => config('app.url')]]
+                        ]
+                    ],
+                ]
+            ])
+        };
 
         return 'ok';
     }
