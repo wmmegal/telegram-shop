b88995965f26eca2e2f6e16e2c4ac816dd8e72ad
wmmeg <wmmegal@gmail.com>
2023-09-20 20:07:51 +0300

    refactor(checkout): Add answer for pre checkout query

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -5,20 +5,28 @@ namespace App\Http\Controllers;
 use App\Actions\StartAction;
 use Log;
 use Telegram\Bot\Api;
+use Telegram\Bot\Exceptions\TelegramSDKException;
 
 class TelegramController extends Controller
 {
+    /**
+     * @throws TelegramSDKException
+     */
     public function __invoke(Api $telegram)
     {
         $updates = $telegram->getWebhookUpdate();
         $text = $updates->getMessage()['text'] ?? '';
         $chatId = $updates->getChat()['id'] ?? '';
-        $preCheckouQuery = $updates->preCheckoutQuery;
+        $preCheckoutQueryId = $updates->preCheckoutQuery->get('id');
 
-        Log::info(print_r($updates->preCheckoutQuery, true));
+        Log::info($preCheckoutQueryId);
 
         match (true) {
-            $text === '/start' => (new StartAction())->handle($chatId)
+            $text === '/start' => (new StartAction())->handle($chatId),
+            $preCheckoutQueryId => $telegram->answerPreCheckoutQuery([
+                'pre_checkout_query_id' => $preCheckoutQueryId,
+                'ok' => true,
+            ])
         };
 
         return 'ok';
